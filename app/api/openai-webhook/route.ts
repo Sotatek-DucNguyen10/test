3266a6e98890a2f2f865e2b50035801db47f33b8
Sotatek-DucNguyen10 <duc.nguyen10@sotatek.com>
2023-11-15 23:13:23 +0700

    Add

--- app/api/openai-webhook/route.ts
+++ app/api/openai-webhook/route.ts
@@ -2,8 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const payload = req.body;
+  const data = await req.json();
+
+  console.log("🚀 ~ POST ~ payload:", payload);
+  console.log("🚀 ~ POST ~ data:", data);
+
   try {
-    return NextResponse.json(payload);
+    return NextResponse.json(data, { status: 200 });
   } catch (e) {
     return NextResponse.json(e);
   }
